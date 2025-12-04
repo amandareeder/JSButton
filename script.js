549cd8ca8fe53207b81605e444a3fb92e3ddc748
amandareeder <reederamanda08@gmail.com>
2025-12-04 10:03:36 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,47 +1,19 @@
-const countSpan = document.getElementById('count');
-const incrementButton = document.getElementById('incrementButton');
-//added a delay to make the speeding up noticeable
-let count = 0;
-let intervalId;
-let currentDelay = 200; // First delay in order to hold button
-const speedUpFactor = 0.8; // Reduce delay when holding
+// make sure these match the HTML element names for the ID then class.
+let input = document.querySelector("#name"); */also put this in lowercase*/
+let display = document.querySelector(".display"); /*put this in lowercase*/
 
-function updateCountDisplay() {
-    countSpan.textContent = count;
+// This code should be working (uncommented input and display as well as bottom line)
+input.addEventListener("input", function () {
+const name = input.value;
+display.textContent = name;
+ }); 
+
+// missing function?
+
+// you should see 5 in the output of your console(Fixed by adding a function :D)
+
+function simpleMath(a, b) {
+  return a + b;
 }
 
-function increment() {
-    count++;
-    updateCountDisplay();
-}
-
-incrementButton.addEventListener('click', () => {
-    // normal click
-    if (!intervalId) {
-        increment();
-    }
-});
-
-incrementButton.addEventListener('mousedown', () => {
-    //holding
-    currentDelay = 200; // Reset delay for new hold
-    intervalId = setInterval(() => {
-        increment();
-        // Speed up 
-        currentDelay = Math.max(50, currentDelay * speedUpFactor); // Delay
-        clearInterval(intervalId);
-        intervalId = setInterval(increment, currentDelay); 
-    }, currentDelay);
-});
-
-incrementButton.addEventListener('mouseup', () => {
-    clearInterval(intervalId);
-    intervalId = null;
-});
-
-incrementButton.addEventListener('mouseleave', () => {
-    clearInterval(intervalId);
-    intervalId = null;
-});
-
-updateCountDisplay(); //make this all visible
+console.log(simpleMath(2, 3));
